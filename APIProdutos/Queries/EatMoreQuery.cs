9925466f3cce3d1643fb37bbf65732441d2c1c6e
Thiagtao da Silva Adriano <tadriano@band.com.br>
2018-11-27 08:12:14 -0200

    add graphql

---
+++ APIProdutos/Queries/EatMoreQuery.cs
@@ -0,0 +1,27 @@
+﻿using APIProdutos.Data;
+using APIProdutos.Models.GraphQL;
+using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIProdutos.Queries
+{
+    public class EatMoreQuery : ObjectGraphType
+    {
+        public EatMoreQuery(ApplicationDbContext db)
+        {
+
+            Field<ListGraphType<ProdutoType>>(
+                "produtos",
+                resolve: context =>
+                {
+                    var produtos = db
+                    .Produtos;
+                    return produtos;
+                });
+
+        }
+    }
+}
